e4ba3db0b74350f89b9a5f68dbe7374e3174a50e
BroStashOf1987inSchool <80268837+BroStashOf1987inSchool@users.noreply.github.com>
2021-12-21 12:19:37 -0500

    Weapons

---
+++ BlueTeamGroupProject/Weapon.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueTeamGroupProject
+{
+    class Weapon
+    {
+        public int Attack
+        {
+            get
+            {
+                return Attack;
+            }
+            set
+            {
+                Attack = value;
+            }
+        }
+
+        public string Item
+        {
+            get
+            {
+                return Item;
+            }
+            set
+            {
+                Item = value;
+            }
+        }
+    }
+}
