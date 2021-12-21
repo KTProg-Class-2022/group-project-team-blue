f1b5a2a1e4a90760f10cd659bd73060d29401237
BroStashOf1987inSchool <80268837+BroStashOf1987inSchool@users.noreply.github.com>
2021-12-21 12:13:12 -0500

    Test

---
+++ Simple Base Code/Weapon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simple_Base_Code
+{
+    class Weapon
+    {
+        public int Attack
+        { 
+            get
+            {
+                return Attack;
+            }
+            set
+            {
+                Attack = value;
+            }
+        }
+
+        public string Item
+        {
+            get
+            {
+                return Item;
+            }
+            set
+            {
+                Item = value;
+            }
+        }
+    }
+}
