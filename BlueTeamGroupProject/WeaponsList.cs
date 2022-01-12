dbc675f191df448163ee29209ebb9a015546efb4
BroStashOf1987inSchool <80268837+BroStashOf1987inSchool@users.noreply.github.com>
2022-01-12 12:07:56 -0500

    thinky

---
+++ BlueTeamGroupProject/WeaponsList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueTeamGroupProject
+{
+    class WeaponsList
+    {
+        // Sword, 5
+        // Mace, 10
+        // Axe, 15
+        // Club, 3
+        // Stick, 1
+    }
+}
