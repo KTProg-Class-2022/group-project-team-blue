a333ba1c4524d8cc62139fdb8657008227ea8a35
Evanrosato <80268717+Evanrosato@users.noreply.github.com>
2022-01-24 11:54:22 -0500

    meas

--- BlueTeamGroupProject/Form1.Designer.cs
+++ BlueTeamGroupProject/Form1.Designer.cs
@@ -29,45 +29,45 @@ namespace BlueTeamGroupProject
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.myConsole = new System.Windows.Forms.RichTextBox();
+            this.myScreen = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.myScreen)).BeginInit();
             this.SuspendLayout();
             // 
-            // richTextBox1
+            // myConsole
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 300);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(776, 138);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
+            this.myConsole.Location = new System.Drawing.Point(12, 300);
+            this.myConsole.Name = "myConsole";
+            this.myConsole.Size = new System.Drawing.Size(776, 138);
+            this.myConsole.TabIndex = 0;
+            this.myConsole.Text = "";
             // 
-            // pictureBox1
+            // myScreen
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(776, 282);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.myScreen.Location = new System.Drawing.Point(12, 12);
+            this.myScreen.Name = "myScreen";
+            this.myScreen.Size = new System.Drawing.Size(776, 282);
+            this.myScreen.TabIndex = 1;
+            this.myScreen.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.myScreen);
+            this.Controls.Add(this.myConsole);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myScreen)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox myConsole;
+        private System.Windows.Forms.PictureBox myScreen;
     }
 }
 
