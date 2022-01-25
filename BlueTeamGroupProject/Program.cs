bd9dcf743ec6b365808277c84149f6e974230884
BroStashOf1987inSchool <80268837+BroStashOf1987inSchool@users.noreply.github.com>
2022-01-25 12:38:20 -0500

    Weapons MK2.1

--- BlueTeamGroupProject/Program.cs
+++ BlueTeamGroupProject/Program.cs
@@ -17,7 +17,7 @@ namespace BlueTeamGroupProject
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            Room basic = new Room();
+            Room basic;
 
             Inventory player = new Inventory();
             
